43a1ca767cdc82f156d50c9bb85f2cae1bc6828f
Fagbohungbe Oluwatobi <tfagbohungbe@gmail.com>
2018-09-19 10:23:13 +0100

    LOP-9: Add Parameterizing to Test Base

--- src/test/java/com/w2a/utilities/ExtentManager.java
+++ src/test/java/com/w2a/utilities/ExtentManager.java
@@ -14,10 +14,9 @@ public class ExtentManager {
 		if(extent==null) {
 			
 			extent = new ExtentReports(System.getProperty("user.dir") + "\\target\\surefire-reports\\html\\extent.html",true,DisplayOrder.OLDEST_FIRST);
-			extent.loadConfig(new File (System.getProperty("user.dir") + "src\\test\\resources\\extentconfig\\ReportsConfig.xml"));
+			extent.loadConfig(new File (System.getProperty("user.dir") + "\\src\\test\\resources\\extentconfig\\ReportsConfig.xml"));
 			
 		}
-		
 		return extent;
 	}
 
