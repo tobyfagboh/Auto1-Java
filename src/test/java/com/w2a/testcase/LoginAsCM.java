755bc72dace0c936b0550b45491346861e3a1092
kaka <blackgeeknerd@gmail.com>
2018-09-20 13:55:02 +0100

    LOP-10:Logged In with Invalid email and password

--- src/test/java/com/w2a/testcase/LoginAsCM.java
+++ src/test/java/com/w2a/testcase/LoginAsCM.java
@@ -11,15 +11,15 @@ import com.w2a.base.TestBase;
 public class LoginAsCM extends TestBase {
 
 	@Test
-	public void loginAsAdnin() throws InterruptedException, IOException {
+	public void loginAsCMM() throws InterruptedException, IOException {
 
 		Thread.sleep(3000);
 		log.debug("Log in page Executed");
-		driver.findElement(By.xpath(OR.getProperty("Email"))).sendKeys(OR.getProperty("CmUserEmail"));
+		driver.findElement(By.xpath(OR.getProperty("Email_Xpath"))).sendKeys(OR.getProperty("CmUserEmail_Xpath"));
 
-		driver.findElement(By.xpath(OR.getProperty("Password"))).sendKeys(OR.getProperty("CmPasswordId"));
+		driver.findElement(By.xpath(OR.getProperty("Password_Xpath"))).sendKeys(OR.getProperty("CmPassword_Xpath"));
 
-		driver.findElement(By.xpath(OR.getProperty("ClickSignIn"))).click();
+		driver.findElement(By.xpath(OR.getProperty("ClickSignIn_Xpath"))).click();
 
 		Assert.assertTrue(isElementPresent(By.xpath(OR.getProperty("userManagementTxtXpath"))),
 				"Unable to validate Homepage");
